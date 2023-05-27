37a1533893310d399ea93c461dc6b14203ae4df1
DarianDickerson <DarianMD32@gmail.com>
2023-05-26 23:52:50 -0600

    Commented scoring details of houseScoring

---
+++ js/houseScoring.js
@@ -0,0 +1,42 @@
+/*
+Takes a string of digits valued from 1-6 
+Returns a number representing the score of the roll
+
+SCORING:
+Full Straight 
+    1 2 3 4 5 6 :   1500
+
+Partial Straight
+    1 2 3 4 5 :     750
+    2 3 4 5 6 :     750
+
+Doubles
+    3sets of pairs: 600
+
+3 of a kind
+    2,3,4,5,6 :     100 * dice #
+    1 :             1000
+
+4 of a kind
+    2,3,4,5,6 :     200 * dice # 
+    1 :             2000
+
+5 of a kind
+    2,3,4,5,6 :     400 * dice #     
+    1 :             4000
+
+6 of a kind
+    2,3,4,5,6 :     1000 * dice #     
+    1 :             10000  
+
+Single Scoring die
+    1 :             100
+    5 :             50
+
+*/
+
+function houseScoring(str){
+    let score = 0
+
+    return score
+}
